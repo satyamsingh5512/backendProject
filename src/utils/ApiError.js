b16125c951e8d8ddec6dd7e374496a63cc2dc06a
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2025-04-15 22:00:38 +0530

    N/A

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,24 +1,21 @@
 class ApiError extends Error {
     constructor(
         statusCode,
-        message= "Something went wrong",
+        message = "Something went wrong",
         errors = [],
         stack = ""
-    ){
-        super(message)
-        this.statusCode = statusCode
-        this.data = null
-        this.message = message
+    ) {
+        super(message);
+        this.statusCode = statusCode;
         this.success = false;
-        this.errors = errors
+        this.errors = Array.isArray(errors) ? errors : [errors];
 
         if (stack) {
-            this.stack = stack
-        } else{
-            Error.captureStackTrace(this, this.constructor)
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
         }
-
     }
 }
 
-export {ApiError}
+export default ApiError;
